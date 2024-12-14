cc1ef5806b9f9da7a30191dbe5454fd4a9e0c2be
resilem <resilem@users.noreply.github.com>
2024-12-14 17:15:49 -0600

    Print each component temperature

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use sysinfo::Components;
+
+fn main() {
+    // Components temperature:
+    let components = Components::new_with_refreshed_list();
+    println!("=> components:");
+    for component in &components {
+        println!("{component:?}");
+    }
+}
